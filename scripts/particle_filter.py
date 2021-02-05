795caa45bd04dba01d4681d40b5a71f535a8eccb
Li Arditi <arditil@cs.uchicago.edu>
2021-02-05 14:00:17 -0600

    updated/fixed update estimated robot pose (check if can use components for quaternions

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -332,19 +332,19 @@ class ParticleFilter:
         orientY = 0
         orientZ = 0
         orientW = 0
-        for p in range(0,self.num_particles):
-            xAvg += particle_cloud[p].pose.postion.x
-            yAvg += particle_cloud[p].pose.postion.y
-            orientX += particle_cloud[p].pose.orientation.x
-            orientY += particle_cloud[p].pose.orientation.y
-            orientZ += particle_cloud[p].pose.orientation.z
-            orientW += particle_cloud[p].pose.orientation.w
-        self.robot_estimate.pose.position.x = xAvg/self.num_particles
-        self.robot_estimate.pose.position.y = yAvg/self.num_particles
-        self.robot_estimate.pose.orientation.x = orientX/self.num_particles
-        self.robot_estimate.pose.orientation.y = orientY/self.num_particles
-        self.robot_estimate.pose.orientation.z = orientZ/self.num_particles
-        self.robot_estimate.pose.orientation.w = orientW/self.num_particles
+        for p in self.num_particles:
+            xAvg += p.pose.postion.x
+            yAvg += p.pose.postion.y
+            orientX += p.pose.orientation.x
+            orientY += p.pose.orientation.y
+            orientZ += p.pose.orientation.z
+            orientW += p.pose.orientation.w
+        self.robot_estimate.position.x = xAvg/self.num_particles
+        self.robot_estimate.position.y = yAvg/self.num_particles
+        self.robot_estimate.orientation.x = orientX/self.num_particles
+        self.robot_estimate.orientation.y = orientY/self.num_particles
+        self.robot_estimate.orientation.z = orientZ/self.num_particles
+        self.robot_estimate.orientation.w = orientW/self.num_particles
 
         pass
 
